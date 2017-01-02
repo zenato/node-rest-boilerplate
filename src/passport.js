ab56715df2c10e3ed7a0c8dc23f871436470fa5e
Yeongjin Lee <yeongjinnn@gmail.com>
2017-01-02 16:14:54 +0900

    change to lazy config

--- src/passport.js
+++ src/passport.js
@@ -2,15 +2,17 @@ import passport from 'passport';
 import { Strategy } from 'passport-http-bearer';
 import { User } from './models';
 
-passport.use(new Strategy(async (token, done) => {
-  try {
-    const user = await User.findByToken(token);
-    if (user) {
-      done(null, user);
-    } else {
-      done(null, false);
+export default function () {
+  passport.use(new Strategy(async (token, done) => {
+    try {
+      const user = await User.findByToken(token);
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    } catch (e) {
+      done(e);
     }
-  } catch (e) {
-    done(e);
-  }
-}));
+  }));
+}
