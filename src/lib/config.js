98d25495e17fa4262114411a4aca7ad5c34baceb
Yeongjin Lee <yeongjinnn@gmail.com>
2017-01-02 13:08:59 +0900

    Ignore check config file.

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { join } from 'path';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const existsConfig = fs.existsSync(join(process.cwd(), '.env'));
+const existsConfig = process.env.SKIP_CONFIG ? true : fs.existsSync(join(process.cwd(), '.env'));
 
 export {
   isProduction,
