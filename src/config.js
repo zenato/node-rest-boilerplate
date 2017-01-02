691e94e81fff0548ec188a5f59e475d475b8b11f
Yeongjin Lee <yeongjinnn@gmail.com>
2017-01-02 15:25:36 +0900

    invalid option fix.

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import dotenv from 'dotenv';
 
 // Initialize config
-dotenv.config({ slient: true });
+dotenv.config({ silent: true });
 
 const IS_PROD = process.env.NODE_ENV === 'production';
 const EXISTS_CONFIG = process.env.SKIP_CONFIG ? true : fs.existsSync(join(process.cwd(), '.env'));
