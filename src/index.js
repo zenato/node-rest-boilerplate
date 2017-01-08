95496ecbc1592ed5f26608d295439e31746cd201
Yeongjin Lee <yeongjinnn@gmail.com>
2017-01-08 20:29:17 +0900

    apply CORS.

--- src/index.js
+++ src/index.js
@@ -31,6 +31,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.disable('x-powered-by');
 
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(morgan(isProd ? 'combined' : 'dev'));
 app.use(routes);
 
