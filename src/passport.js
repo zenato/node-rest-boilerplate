226833ebeb74297b1613f50e82ff29eb35c3dab3
Yeongjin Lee <yeongjinnn@gmail.com>
2017-01-09 21:00:34 +0900

    Refactor import passport strategy.

--- src/passport.js
+++ src/passport.js
@@ -1,9 +1,9 @@
 import passport from 'passport';
-import { Strategy } from 'passport-http-bearer';
+import BearerStrategy from 'passport-http-bearer';
 import { User } from './models';
 
 export default function () {
-  passport.use(new Strategy(async (token, done) => {
+  passport.use(new BearerStrategy(async (token, done) => {
     try {
       const user = await User.findByToken(token);
       if (user) {
@@ -12,6 +12,7 @@ export default function () {
         done(null, false);
       }
     } catch (e) {
+      console.log(e);
       done(e);
     }
   }));
