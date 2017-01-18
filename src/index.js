6cba8a26c340b9b05117c18099eb1d85e2068990
Yeongjin Lee <yeongjinnn@gmail.com>
2017-01-18 14:48:11 +0900

    Fixed CORS bugs.

--- src/index.js
+++ src/index.js
@@ -34,8 +34,13 @@ app.disable('x-powered-by');
 // CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 app.use(morgan(isProd ? 'combined' : 'dev'));
